69b9a142635f806fc2f91652afa0b98faa5ae609
OldRabbit736 <sylvan0212@gmail.com>
2022-06-10 15:08:04 +0900

    Complete 6.5 Collector interface

---
+++ src/main/java/modernjavainaction/chap06/ToListCollector.java
@@ -0,0 +1,46 @@
+package modernjavainaction.chap06;
+
+import modernjavainaction.chap04.Dish;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
+
+    public static void main(String[] args) {
+        List<Dish> dishes = Dish.menu.stream().collect(new ToListCollector<>());
+        System.out.println(dishes);
+    }
+
+    @Override
+    public Supplier<List<T>> supplier() {
+        return ArrayList::new;
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        return List::add;
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        return (l1, l2) -> {
+            l1.addAll(l2);
+            return l1;
+        };
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        return Function.identity();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH, Characteristics.CONCURRENT));
+    }
+}
