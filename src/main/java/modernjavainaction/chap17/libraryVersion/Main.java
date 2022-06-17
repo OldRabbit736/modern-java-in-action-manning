a632e2f822f40aa899b16504cd9aac5cc691328a
OldRabbit736 <sylvan0212@gmail.com>
2022-06-17 21:20:45 +0900

    Listing 17.12 작성 중에 중단. chapter 17은 이쯤하면 된 듯 싶어 중단.

--- src/main/java/modernjavainaction/chap17/libraryVersion/Main.java
+++ src/main/java/modernjavainaction/chap17/libraryVersion/Main.java
@@ -2,9 +2,28 @@ package modernjavainaction.chap17.libraryVersion;
 
 
 import io.reactivex.rxjava3.core.Observable;
+import modernjavainaction.chap17.implementVersion.TempInfo;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         Observable<String> strings = Observable.just("first", "second");
+        Observable<Long> onePerSec = Observable.interval(1, TimeUnit.SECONDS);
+
+//        onePerSec.subscribe(i -> System.out.println(TempInfo.fetch("New York")));
+        onePerSec.blockingSubscribe(
+                i -> System.out.println(TempInfo.fetch("New York"))
+        );
+
+    }
+
+    public static Observable<TempInfo> getTemperature(String town) {
+        return Observable.create(emitter ->
+                Observable.interval(1, TimeUnit.SECONDS)
+                        .subscribe(i -> {
+
+                        })
+        );
     }
 }
